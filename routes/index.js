ff21d0160a9a19e70c9a4069794adaedf3ed1f96
lastin <prashantisotiya@gmail.com>
2022-09-09 17:05:15 +0530

    added some fake APIs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const NodeCache = require( "node-cache" );
+
+const myCache = new NodeCache();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/:apiName', (req, res) => {
+  myCache.set(req.params.apiName, req.body)
+  res.send({
+    message: 'added'
+  })
+})
+
+router.get('/getAllFake', (req, res) => {
+  const keys = myCache.keys()
+  res.send({
+    data: keys,
+    message: 'success'
+  })
+})
+
+router.get('/:apiName', (req, res) => {
+  const data = myCache.get(req.params.apiName)
+  if (!data) {
+    res.send({
+      data: [],
+      message: "not found any data"
+    })
+  }
+  res.send({
+    data,
+    message: 'success'
+  })
+})
 module.exports = router;
