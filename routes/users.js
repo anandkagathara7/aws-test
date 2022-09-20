4c5ae1e4080c3ec6700e5267be62bc8dca3a4653
lastin <prashantisotiya@gmail.com>
2022-09-20 13:37:27 +0530

    added eslint,dotenv and sequelize support

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello test');
-});
+router.get('/', (req, res, next) => {
+  res.send('Hello test')
+})
 
-module.exports = router;
+module.exports = router
