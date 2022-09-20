c192d169b20f12b5e7420044986858c63210dba4
lastin <prashantisotiya@gmail.com>
2022-09-20 17:48:35 +0530

    added aws rds database link to custom response

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,48 @@
 const express = require('express')
 const router = express.Router()
-const NodeCache = require('node-cache')
-
-const myCache = new NodeCache()
-
+const db = require('../database/models')
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' })
 })
 
-router.post('/:apiName', (req, res) => {
-  myCache.set(req.params.apiName, req.body)
+router.post('/:apiName', async (req, res) => {
+  await db.customResponse.create({
+    slug: req.params.apiName,
+    response: req.body
+  })
   res.send({
     message: 'added'
   })
 })
 
-router.get('/getAllFake', (req, res) => {
-  const keys = myCache.keys()
+router.get('/getAllFake', async (req, res) => {
+  let data = await db.customResponse.findAll({
+    attributes: ['slug']
+  })
+  data = data.map((item) => item.slug)
   res.send({
-    data: keys,
+    data,
     message: 'success'
   })
 })
 
-router.get('/:apiName', (req, res) => {
-  const data = myCache.get(req.params.apiName)
+router.get('/:apiName', async (req, res) => {
+  const data = await db.customResponse.findOne({
+    where: {
+      slug: req.params.apiName
+    }
+  })
   if (!data) {
     res.send({
       data: [],
       message: 'not found any data'
     })
+  } else {
+    res.send({
+      data: data.response,
+      message: 'success'
+    })
   }
-  res.send({
-    data,
-    message: 'success'
-  })
 })
 module.exports = router
